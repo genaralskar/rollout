9d7049a270f4faadcddbc5f764e61088f5f4d3a0
Unknown <genaralskar1@gmail.com>
2018-05-16 17:48:13 -0600

    Add procedural level generation

---
+++ TransformGame/Assets/Collin Scripts/Scripts/EventManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventManager : MonoBehaviour
+{
+
+	public enum HeightStates{Car, Robot, Plane}
+	public static event UnityAction<HeightStates> PlayerChangedHeight = delegate { };
+
+	public void ChangePlayerHeightState(string newState)
+	{
+		switch (newState)
+		{
+			case "Car":
+				PlayerChangedHeight(HeightStates.Car);
+				break;
+			case "Robot":
+				PlayerChangedHeight(HeightStates.Robot);
+				break;
+			case "Plane":
+				PlayerChangedHeight(HeightStates.Plane);
+				break;
+			default:
+				break;
+		}
+	}
+}
---
+++ TransformGame/Assets/Collin Scripts/Scripts/PlayerHeightController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Messaging;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+namespace genaralskar
+{
+	public class PlayerHeightController : MonoBehaviour {
+	
+		public float transitionDuration = 1f;
+		public float carHeight = 2;
+		public float robotHeight = 5;
+		public float planeHeight = 10;
+
+		private void Start()
+		{
+			EventManager.PlayerChangedHeight += ChangeState;
+		}
+
+		public void ChangeState(EventManager.HeightStates state)
+		{
+			float height = 0;
+			switch (state)
+			{
+				case EventManager.HeightStates.Car:
+					height = carHeight;
+					break;
+				case EventManager.HeightStates.Robot:
+					height = robotHeight;
+					break;
+				case EventManager.HeightStates.Plane:
+					height = planeHeight;
+					break;
+				default:
+					break;
+			}
+			StopAllCoroutines();
+			StartCoroutine(ChangeHeight(height));
+		}
+	
+		private IEnumerator ChangeHeight(float height)
+		{
+			float timer = 0;
+			float transitionTime = 0;
+			while (transitionTime < 1)
+			{
+				transitionTime = timer / transitionDuration;
+				Vector3 newPos = new Vector3(transform.position.x, Mathf.SmoothStep(transform.position.y, height, timer), transform.position.z);
+				transform.position = newPos;
+				timer += Time.deltaTime;
+				
+				print("Moving playa");
+				yield return new WaitForEndOfFrame();
+			}
+
+			transform.position = new Vector3(transform.position.x, height, transform.position.z);
+		}
+	}
+}
+
